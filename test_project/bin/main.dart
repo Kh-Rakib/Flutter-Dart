abca04acf1d9d52eb055ab1c790eb48b0bb33d3e
Kh-Rakib <52630858+Kh-Rakib@users.noreply.github.com>
2020-05-13 14:03:02 +0600

    A test folder where dart language will be practiced

---
+++ test_project/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:test_project/test_project.dart' as test_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_project.calculate()}!');
+}
---
+++ test_project/test/test_project_test.dart
@@ -0,0 +1,3 @@
+void main() {
+  print('Hello world this is me Dart');
+}
