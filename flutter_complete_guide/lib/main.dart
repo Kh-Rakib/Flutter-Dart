9fd90b61f260136037de137f8240545a35006e0b
Kh-Rakib <52630858+Kh-Rakib@users.noreply.github.com>
2020-05-16 21:30:12 +0600

    File:Flutter Complete Guide Added a function to run and print Hello to The Screen

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,13 +1,26 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
+//import some files or getting access from files of SDK
 
 void main() {
+//This is the main function when app starts this will
+//Get called first or Execute first
 
-
+runApp(MyApp());
+//runApp is a function provided by material.dart file
+//This function will help to render 
+//Onto the screen by calling constructor
 }
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+  //MyApp is a custom made class that inherit StatelessWidget
+
+  Widget build(BuildContext context) {  
+    //Widget is the type here build will return something which is a Widget
+    //Context here is an Argument
+      return MaterialApp(home: Text('Hello!'),);
+      //MaterialApp is a class
+      //Home is a Argument
+      //Text here is a Object provided by maaterial.dart
   }
   
 }
