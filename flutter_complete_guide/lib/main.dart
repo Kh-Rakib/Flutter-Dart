0731edfbb6617e006e3e8c6f75a6f47ed008626c
Kh-Rakib <52630858+Kh-Rakib@users.noreply.github.com>
2020-06-08 20:50:31 +0600

    Change states of questions

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -8,17 +8,46 @@ void main() {
   //runApp is function which provided by material.dart
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+class MyAppState extends State<MyApp> {
+  var questionIndex =0 ;
+
+  void amswerQuestion() {
+    setState((){
+    questionIndex = questionIndex + 1;
+
+    }); 
+ 
+    print(questionIndex);
+  }
   @override
   //We make a class MyApp which inherit another class which is provided by material.dart
   Widget build(BuildContext context) {
     //build is a method here with type:Widget and context is a argument ,This argument will call by flutter
+    var question = [ 'What is You Name',
+    'What is youor favourirte color?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('My First App')
         ),
-        body: Text('Hello World\'s'),
+        body: Column(
+          children: <Widget>[
+            Text(question[questionIndex]),
+            RaisedButton(child: Text('Answer 1'), onPressed: amswerQuestion), //BUTTON
+            Text('Question 2 '),
+            RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answer 2 chosen'),), //BUTTON //Here ()= is an anonymous function
+             Text('Question 3 '),
+            RaisedButton(child: Text('Answer 3'), onPressed: amswerQuestion), //BUTTON
+          ],
+        ),
       ),
     );
     //buuild method should return something
