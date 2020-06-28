1f4d9836e09d5c5685d6bb74381f6b21e66eb807
Kh-Rakib <52630858+Kh-Rakib@users.noreply.github.com>
2020-06-28 21:59:09 +0600

    Added a new class fine called Transactions also added list to the main file

--- Expense_Planner/lib/main.dart
+++ Expense_Planner/lib/main.dart
@@ -1,3 +1,4 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,6 +14,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [  // Here <Transactions> is ised as type of the list 
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,13 +35,15 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
             child: Card(
               color: Colors.blue,
               child: Text('CHART!'),
-              elevation: 5,
+              elevation: 5, // Shadow under the card aka Hover Effect
             ),
           ),
           Card(
@@ -37,4 +54,3 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
- 
