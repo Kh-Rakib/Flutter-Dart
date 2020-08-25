f48f4e1d938f27dc424fd200dc1119eb4b567988
Kh-Rakib <52630858+Kh-Rakib@users.noreply.github.com>
2020-08-25 23:51:54 +0600

    Styling Text , More Styling

--- Expense_Planner/lib/main.dart
+++ Expense_Planner/lib/main.dart
@@ -63,20 +63,39 @@ class MyHomePage extends StatelessWidget {
                     ),
                     decoration: BoxDecoration(
                       border: Border.all(
-                        color: Colors.black,
+                        color: Colors.purple,
                         width: 2,
-                      ), 
+                      ),
                     ),
                     padding: EdgeInsets.all(10),
                     child: Text(
                       tx.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(tx.title),
+                       Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple[200],
+                          ),
+                        ),
+                      
                       Text(
                         tx.date.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
                       ),
                     ],
                   ),
